9ab6f408021349747f60b183d637e1db31db94a4
cachtayl <54186417+cachtayl@users.noreply.github.com>
2022-08-24 19:46:26 -0700

    MVP

--- list.py
+++ list.py
@@ -1,26 +1,28 @@
+from ast import Continue
 import json
 import os.path
-from typing import OrderedDict
-from unicodedata import category
 
 def main():
+    os.system('clear')
     # if json db does not exist create one
     if os.path.exists('stores.json') == False: 
         file = open("stores.json", "w")
         file.write('[]')
         file.close()
     
-    options = input("Welcome to the Main Menu!\nPress 0 if you want to create an efficient grocery list\nPress 1 if you want to register a store\n")
+    options = input("Welcome to the Main Menu!\n0 : Create an efficient grocery list\n1 : Register a store\n0/1 : ")
     if(options == '0'):
         makeList()
     else:
         registerStore()
         
 def registerStore():
+    os.system('clear')
     store_name = input("Enter the name of the store: ")
     new_store = {"store_name": store_name}
-   
-    print("Great! Now please enter all of the aisle names and the corresponding row number")
+    
+    os.system('clear')
+    print("Enter the aisle names and their row number\n")
 
     file = open("stores.json", "r")
     content = file.read()
@@ -28,12 +30,12 @@ def registerStore():
     
     aisle_name = ''
     while(True):
-        aisle_name = input("Aisle name: ")
-        if(aisle_name == "Done"):
-            break
-        number = input("Aisle number: ")
+        aisle_name = input("Category: ")
+        number = input("Row: ")
         new_store[number] = aisle_name
-    print(new_store)
+        if input("Continue? y/n: ") == "n":
+            break
+        print("\n")
 
     stores.append(new_store)
     
@@ -43,48 +45,55 @@ def registerStore():
     file.write(json_obj)
 
     file.close()
+    main()
 
 def makeList():
+    os.system('clear')
     file = open("stores.json", "r")
 
     content = file.read()
     stores = json.loads(content)
-    print("\nRegistered Stores")
     for idx,store in enumerate(stores):
         print(idx , ":" , store['store_name'])
-    choice = input("Please select the store you are going to: ")
+    choice = input("Choose a Store: ")
 
+    os.system('clear')
     store = stores[int(choice)]
     
-    print(store['store_name'] + " has aisles: ")
+    print("\n" + store['store_name'] + " has aisles: ")
     aisles = ""
     for aisle in sorted(store.keys()):
         num = store[aisle]
         if(aisle != "store_name"):
-            aisles += "\n"+ aisle + ": "+ num
+            aisles += aisle + ": "+ num + "\n"
     print(aisles)
-    print("Now enter your future items and their matching aisle number")
-    print("Enter 'Done' when you are finished")
+    print("Enter your items and it's category\n")
     shopping_list = {}
     while(True):
-        item_name = input("Item name: ")
-        if(item_name == "Done"):
-            break
-        number = input("Associated aisle: ")
+        item_name = input("Item: ")
+        number = input("Row: ")
         if number in shopping_list:
             shopping_list[number].append(item_name)
         else:
             shopping_list[number] = [item_name]
+        
+        if input("Continue? y/n: ") == "n":
+            break
+        print("\n")
     
-    orderedList = "Shopping with efficiency!\n"
+    # Making the list 
+    orderedList = ""
     for row in sorted(shopping_list.keys()):
         for item in shopping_list[row]:
             orderedList += item + "\n"
+    
+    os.system('clear')
 
     file_name = input("Name your file: ") + ".txt"
     file = open(file_name, "w")
     file.write(orderedList)
     file.close()
+
 if __name__ == "__main__":
     main()
 
