b8bf5a9187aa0319f0e44ff3c45824a3d9a7fe3f
cachtayl <54186417+cachtayl@users.noreply.github.com>
2022-10-03 16:40:36 -0700

    combobox changes

--- pyside_list.py
+++ pyside_list.py
@@ -1,38 +1,44 @@
-import json, os, re, sys
+from PySide2.QtGui import QIcon, QFont, QPalette, QColor
+from PySide2.QtWidgets import *
+from PySide2.QtCore import QSize, Qt
+import json
+import os
+import re
+import sys
 import faulthandler
 faulthandler.enable()
 
-from PySide2.QtCore import QSize, Qt
-from PySide2.QtWidgets import *
-from PySide2.QtGui import QIcon, QFont, QPalette, QColor
 
 class MainWindow(QMainWindow):
     def __init__(self):
         # if json db does not exist, then create one
-        if os.path.exists('stores.json') == False: 
+        if os.path.exists('stores.json') == False:
             db = open("stores.json", "w")
             db.write('[]')
             db.close()
-        #initialize list of stores
+        # initialize list of stores
         self.update_stores()
-        #BUG will break if stores is empty
+        # BUG will break if stores is empty
         # self.store = self.stores[0]
-    
+
         self.initUI()
 
     def initUI(self):
         super().__init__()
         font = self.font()
-        font.setPointSize(10)
+        font.setPointSize(12)
         self.window().setFont(font)
-        
+
         self.stacklayout = QStackedLayout()
+
+        self.menuTab()  # stack index0
+        self.registerTab()  # stack index1
         
-        self.menuTab()          #stack index0
-        self.registerTab()      #stack index1
         self.setWindowTitle("Main Menu")
         self.resize(300, 300)
-        
+        self.toolbar.toggleViewAction().setChecked(False)
+        self.toolbar.toggleViewAction().trigger()
+
         widget = QWidget()
         widget.setLayout(self.stacklayout)
         self.setCentralWidget(widget)
@@ -44,29 +50,30 @@ class MainWindow(QMainWindow):
         self.toolbar.setOrientation(Qt.Vertical)
         self.addToolBar(Qt.LeftToolBarArea, self.toolbar)
         self.toolbar.setIconSize(QSize(20, 20))
-        self.toolbar.toggleViewAction().setChecked(False)
-        self.toolbar.toggleViewAction().trigger()
-        
-        #Plus icon by Icons8: https://icons8.com/icon/3QiOfjluKyC9/plus
-        register_btn = QAction(QIcon("./icons/icons8-plus-24.png"), "Add Store", self)
+    
+        # Plus icon by Icons8: https://icons8.com/icon/3QiOfjluKyC9/plus
+        register_btn = QAction(
+            QIcon("./icons/icons8-plus-24.png"), "Add Store", self)
         register_btn.setToolTip("Add a Store")
         register_btn.triggered.connect(lambda: self.changeTab(1))
         self.toolbar.addAction(register_btn)
-        
-        #Delete icon by Icons8: https://icons8.com/icon/lenQJDeFgQWn/minus
-        delete_btn = QAction(QIcon("./icons/icons8-subtract-24.png"), "Delete Store", self)
+
+        # Delete icon by Icons8: https://icons8.com/icon/lenQJDeFgQWn/minus
+        delete_btn = QAction(
+            QIcon("./icons/icons8-subtract-24.png"), "Delete Store", self)
         delete_btn.setToolTip("Delete selected Store")
         delete_btn.triggered.connect(self.delete_store)
         self.toolbar.addAction(delete_btn)
         delete_btn.setEnabled(False)
-        
-        #Edit icon by Icons8: "https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"
-        edit_btn = QAction(QIcon("./icons/icons8-edit-24.png"), "Edit Store", self)
+
+        # Edit icon by Icons8: "https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"
+        edit_btn = QAction(
+            QIcon("./icons/icons8-edit-24.png"), "Edit Store", self)
         edit_btn.setToolTip("Edit selected Store")
         edit_btn.triggered.connect(self.edit_store)
         self.toolbar.addAction(edit_btn)
         edit_btn.setEnabled(False)
-        
+
         self.storesListWidget = QListWidget()
 
         for store in self.stores:
@@ -78,9 +85,9 @@ class MainWindow(QMainWindow):
         list_btn.pressed.connect(lambda: self.changeTab(2))
         list_btn.setEnabled(False)
         main_menu_layout.addWidget(list_btn)
-        
-        #enable 'Make Shopping List' button if there is a selection
-        def enable():  
+
+        # enable 'Make Shopping List' button if there is a selection
+        def enable():
             if len(self.storesListWidget.selectedItems()) != 0:
                 list_btn.setEnabled(True)
                 delete_btn.setEnabled(True)
@@ -90,63 +97,70 @@ class MainWindow(QMainWindow):
                 delete_btn.setEnabled(False)
                 edit_btn.setEnabled(False)
         self.storesListWidget.itemSelectionChanged.connect(enable)
-            
+
         menu_widget = QWidget()
         menu_widget.setLayout(main_menu_layout)
         self.stacklayout.addWidget(menu_widget)
+
     def resetMenuTab(self):
         self.storesListWidget.clear()
         for store in self.stores:
             listWidgetItem = QListWidgetItem(store['store_name'])
             self.storesListWidget.addItem(listWidgetItem)
+
     def update_stores(self):
-        #update the stores list 
+        # update the stores list
         db = open("stores.json", "r")
         content = db.read()
         self.stores = json.loads(content)
         db.close()
-    
+
     def delete_store(self):
         self.update_stores()
         selected = self.storesListWidget.currentItem()
         selected_idx = self.storesListWidget.row(selected)
         del self.stores[selected_idx]
-        #overwrite json file with the new stores list
+        # overwrite json file with the new stores list
         json_obj = json.dumps(self.stores, indent=4)
         db = open("stores.json", "w")
         db.write(json_obj)
         db.close()
-        
+
         self.storesListWidget.takeItem(selected_idx)
+
     def edit_store(self):
         self.update_stores()
-        self.store = self.stores[self.storesListWidget.row(self.storesListWidget.currentItem())]
-        self.setWindowTitle("Edit "+ self.store['store_name'])
+        self.store = self.stores[self.storesListWidget.row(
+            self.storesListWidget.currentItem())]
+        self.setWindowTitle("Edit " + self.store['store_name'])
         self.resize(500, 300)
         self.resetRegisterTab()
         self.store_name.setText(self.store['store_name'])
-        
+
         self.store.pop('store_name')
         for aisle_num in self.store.keys():
             rowPos = self.registerTableWidget.rowCount()
             category = self.store[aisle_num]
             for elem in category:
                 self.registerTableWidget.insertRow(rowPos)
-                self.registerTableWidget.setItem(rowPos, 0, MyTableWidgetItem(str(aisle_num)))
-                self.registerTableWidget.setItem(rowPos, 1, MyTableWidgetItem(elem))
+                self.registerTableWidget.setItem(
+                    rowPos, 0, MyTableWidgetItem(str(aisle_num)))
+                self.registerTableWidget.setItem(
+                    rowPos, 1, MyTableWidgetItem(elem))
         self.delete_store()
         self.toolbar.toggleViewAction().trigger()
+        self.resize(1000, 600)
         self.stacklayout.setCurrentIndex(1)
 
     def registerTab(self):
         # self.toolbar.toggleViewAction().trigger()
-        register_layout = QVBoxLayout() 
+        register_layout = QVBoxLayout()
         aisle_layout = QVBoxLayout()
-        
+
         name_label = QLabel("Store Name:")
         self.store_name = QLineEdit()
         self.store_name.setPlaceholderText("E.g. Walmart")
-        
+
         name_layout = QHBoxLayout()
         name_layout.addWidget(name_label)
         name_layout.addWidget(self.store_name)
@@ -155,7 +169,7 @@ class MainWindow(QMainWindow):
         row_label = QLabel("Row:")
         self.row = QSpinBox()
         self.row.setMinimum(1)
-        
+
         category_label = QLabel("Contains:")
         self.category = QLineEdit()
         self.category.setPlaceholderText("E.g. Desserts/Pasteries")
@@ -165,12 +179,14 @@ class MainWindow(QMainWindow):
         row_layout.addWidget(self.row)
         row_layout.addWidget(category_label)
         row_layout.addWidget(self.category)
-        
+
         self.registerTableWidget = QTableWidget()
         self.registerTableWidget.setColumnCount(2)
         self.registerTableWidget.verticalHeader().setVisible(False)
-        self.registerTableWidget.setHorizontalHeaderItem(0, MyTableWidgetItem("Row"))
-        self.registerTableWidget.setHorizontalHeaderItem(1, MyTableWidgetItem("Category"))
+        self.registerTableWidget.setHorizontalHeaderItem(
+            0, MyTableWidgetItem("Row"))
+        self.registerTableWidget.setHorizontalHeaderItem(
+            1, MyTableWidgetItem("Category"))
         self.registerTableWidget.setColumnWidth(0, 20)
         self.registerTableWidget.horizontalHeader().setStretchLastSection(True)
 
@@ -178,11 +194,14 @@ class MainWindow(QMainWindow):
             if self.category.text():
                 rowPos = self.registerTableWidget.rowCount()
                 self.registerTableWidget.insertRow(rowPos)
-                self.registerTableWidget.setItem(rowPos, 0, MyTableWidgetItem(str(self.row.value())))
-                self.registerTableWidget.setItem(rowPos, 1, MyTableWidgetItem(self.category.text()))
+                self.registerTableWidget.setItem(
+                    rowPos, 0, MyTableWidgetItem(str(self.row.value())))
+                self.registerTableWidget.setItem(
+                    rowPos, 1, MyTableWidgetItem(self.category.text()))
                 self.registerTableWidget.sortItems(0, Qt.AscendingOrder)
                 self.category.clear()
-            else: None
+            else:
+                None
         self.category.returnPressed.connect(add_aisle)
         add_btn = QPushButton("Add Aisle")
         add_btn.pressed.connect(add_aisle)
@@ -199,7 +218,7 @@ class MainWindow(QMainWindow):
                 category = self.registerTableWidget.item(i, 1)
                 category = category.text()
                 new_store.setdefault(row, []).append(category)
-            
+
             self.stores.append(new_store)
             json_obj = json.dumps(self.stores, indent=4)
             db = open("stores.json", "w")
@@ -209,7 +228,7 @@ class MainWindow(QMainWindow):
 
         reg_btn = QPushButton("Register")
         reg_btn.pressed.connect(insertStore)
-        
+
         def cancel():
             if 'Edit' in self.windowTitle():
                 insertStore()
@@ -217,17 +236,17 @@ class MainWindow(QMainWindow):
                 self.changeTab(0)
         cancel_btn = QPushButton("Cancel")
         cancel_btn.pressed.connect(cancel)
-        
+
         btn_layout = QHBoxLayout()
         btn_layout.addWidget(reg_btn)
         btn_layout.addWidget(cancel_btn)
         register_layout.addLayout(aisle_layout)
         register_layout.addLayout(btn_layout)
-        
+
         register_widget = QWidget()
         register_widget.setLayout(register_layout)
         self.stacklayout.addWidget(register_widget)
-    
+
     def resetRegisterTab(self):
         self.store_name.clear()
         self.row.setValue(1)
@@ -239,32 +258,39 @@ class MainWindow(QMainWindow):
         shopping_list_layout = QVBoxLayout()
 
         nest_layout = QVBoxLayout()
-        
+
         item_layout = QHBoxLayout()
         item_label = QLabel("Item:")
         self.user_item = QLineEdit()
         self.user_item.setPlaceholderText("E.g. Bananas")
         
+        cat_layout = QHBoxLayout()
         categories_label = QLabel("Associated Aisle: ")
         self.categories = QComboBox()
-        self.categories.setPlaceholderText("")
-        
-        self.store = self.stores[self.storesListWidget.row(self.storesListWidget.currentItem())]
+
+
+        self.store = self.stores[self.storesListWidget.row(
+            self.storesListWidget.currentItem())]
         self.nums = []
         self.store.pop('store_name')
         for aisle_num in self.store.keys():
             category = self.store[aisle_num]
+            tot = ''
             for element in category:
-                self.categories.addItem(element)
-                self.nums.append(aisle_num)
+                tot += element + ' / '
+            self.categories.addItem(tot[:-2])
+            self.nums.append(aisle_num)
         self.categories.setCurrentIndex(-1)
-        
+
         self.shoppingTableWidget = QTableWidget()
         self.shoppingTableWidget.setColumnCount(3)
         self.shoppingTableWidget.verticalHeader().setVisible(False)
-        self.shoppingTableWidget.setHorizontalHeaderItem(0, MyTableWidgetItem("Row"))
-        self.shoppingTableWidget.setHorizontalHeaderItem(1, MyTableWidgetItem("Item"))
-        self.shoppingTableWidget.setHorizontalHeaderItem(2, MyTableWidgetItem("Aisle"))
+        self.shoppingTableWidget.setHorizontalHeaderItem(
+            0, MyTableWidgetItem("Row"))
+        self.shoppingTableWidget.setHorizontalHeaderItem(
+            1, MyTableWidgetItem("Item"))
+        self.shoppingTableWidget.setHorizontalHeaderItem(
+            2, MyTableWidgetItem("Aisle"))
         self.shoppingTableWidget.setEditTriggers(QTableWidget.NoEditTriggers)
         self.shoppingTableWidget.setColumnWidth(0, 20)
         self.shoppingTableWidget.setColumnWidth(1, 220)
@@ -276,32 +302,37 @@ class MainWindow(QMainWindow):
                 selectedAisle_num = self.nums[self.categories.currentIndex()]
                 rowPos = self.shoppingTableWidget.rowCount()
                 self.shoppingTableWidget.insertRow(rowPos)
-                self.shoppingTableWidget.setItem(rowPos, 0, MyTableWidgetItem(selectedAisle_num))
-                self.shoppingTableWidget.setItem(rowPos, 1, MyTableWidgetItem(self.user_item.text()))
-                self.shoppingTableWidget.setItem(rowPos, 2, MyTableWidgetItem(selectedAisle_name))
+                self.shoppingTableWidget.setItem(
+                    rowPos, 0, MyTableWidgetItem(selectedAisle_num))
+                self.shoppingTableWidget.setItem(
+                    rowPos, 1, MyTableWidgetItem(self.user_item.text()))
+                self.shoppingTableWidget.setItem(
+                    rowPos, 2, MyTableWidgetItem(selectedAisle_name))
                 self.shoppingTableWidget.resizeColumnToContents(0)
                 self.shoppingTableWidget.sortItems(0, Qt.AscendingOrder)
                 self.user_item.clear()
                 self.categories.setCurrentIndex(-1)
-            else: None 
+            else:
+                None
         self.user_item.returnPressed.connect(add_item)
         add_btn = QPushButton("Add Item")
         add_btn.pressed.connect(add_item)
-        
+
         item_layout.addWidget(item_label)
         item_layout.addWidget(self.user_item)
-        item_layout.addWidget(categories_label)
-        item_layout.addWidget(self.categories)
+        cat_layout.addWidget(categories_label)
+        cat_layout.addWidget(self.categories)
         nest_layout.addLayout(item_layout)
+        nest_layout.addLayout(cat_layout)
         nest_layout.addWidget(add_btn)
         shopping_list_layout.addLayout(nest_layout)
 
         cancel_btn = QPushButton("Cancel")
         cancel_btn.pressed.connect(lambda: self.changeTab(0))
-        
+
         shopping_list_layout.addWidget(self.shoppingTableWidget)
         shopping_list_layout.addWidget(cancel_btn)
-        
+
         shopping_list = QWidget()
         shopping_list.setLayout(shopping_list_layout)
         self.stacklayout.addWidget(shopping_list)
@@ -312,28 +343,27 @@ class MainWindow(QMainWindow):
         # Leaving menu tab
         if self.stacklayout.currentIndex() == 0:
             self.toolbar.toggleViewAction().trigger()
-
-        # Leaving shoppingList tab
+        # Leaving shoppingList tab, will delete that page and recreate on demand
         elif self.stacklayout.currentIndex() == 2:
             currShoppingList = self.stacklayout.currentWidget()
             currShoppingList.deleteLater()
-        
+
         # Switching between pages of the stacklayout
         if page_idx == 0:
             self.setWindowTitle("Main Menu")
             self.resize(300, 300)
             self.toolbar.toggleViewAction().trigger()
             self.resetMenuTab()
-            
         elif page_idx == 1:
             self.setWindowTitle("Register Store")
-            self.resize(500, 300)
+            self.resize(1000, 600)
             self.resetRegisterTab()
         elif page_idx == 2:
-            self.shoppingListTab()  #stack index2
+            #create on this tab on demand
+            self.shoppingListTab()
             store_name = self.storesListWidget.currentItem()
             self.setWindowTitle(store_name.text()+" Shopping List")
-            self.resize(500, 300)
+            self.resize(1000, 600)
 
         self.stacklayout.setCurrentIndex(page_idx)
 
@@ -346,8 +376,6 @@ class MyTableWidgetItem(QTableWidgetItem):
             return QTableWidgetItem.__lt__(self, other)
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
-
 app.exec_()
